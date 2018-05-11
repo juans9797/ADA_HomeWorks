4b1eadd35e9abc32f357f71f3b0e8db6105d6400
juans9797 <juansebastianrivera@hotmail.com>
2018-05-10 20:48:27 -0500

    Corrected parser, working over solve2 (which is the unifying algorithm)

--- Proyecto/agents1.py
+++ Proyecto/agents1.py
@@ -56,12 +56,15 @@ def parser(i):
 		var1 = stack.pop()
 		var1.append(var)
 		stack.append(var1)
-	if name != "":
+	if name != "" and len(stack)>=1:
 		var = stack.pop()
 		var.append(name)
 		name = ""
 		stack.append(var)
-
+	elif name != "" and len(stack)==0:
+		temp.append(name)
+		stack.append(temp)
+		name = ""
 	return stack.pop()
 
 
@@ -76,14 +79,45 @@ def solve():
 
 
 def solve2(var1,var2):
-	l = len(var1)
+	l = min(len(var1),len(var2))
+	print(l)
 	for i in range(l-1):
+		#-------------AQUI TOCA REVISAR EL RANGO-------------------#
+		print(i)
 		if isVariable(var1[i]):
-			print("Variable")
-			#print(var1[i])
-		elif not isVariable(var1[i]) and isinstance(var1[i+1],list):
-			print("Funcion")
+			print("--Entro")
+			if not isVariable(var2[i]) and isinstance(var2[i+1],list):
+				var3 = var1[i]
+				print("Entro")
+				for j in var2[i+1]:
+					if isinstance(j,list):
+						if var3 == j:
+							print("Se exploto")
+							return False
+						else:
+							return True
+					else:
+						if var3 == j:
+							return False
+						else:
+							return True
 
+
+		elif not isVariable(var1[i]) and isinstance(var1[i+1],list):
+			if not isVariable(var2[i]) and isinstance(var2[i+1],list):
+				if var1[i] != var2[i]:
+					return False
+				else:
+					print("Aqui toca hacer algo")
+			else:
+				return False
+		elif not isVariable(var1[i]) and not isinstance(var1[i+1],list):
+			if var1[i]==var2[i]:
+				print("Si")
+			else:
+				return False
+		else:
+			return True
 
 def isVariable(a):
 	#Verifica si la variable a que ingreso, esta dentro del ASCII de las
@@ -93,7 +127,6 @@ def isVariable(a):
 		return True
 	else:
 		return False
-
 
 
 
